38f06e4d23aed4a3f63c45f5aabed9c22b72f253
Wondermay <86824314+maggyura@users.noreply.github.com>
2025-12-16 22:20:47 +0400

    Update settings.py

--- budgeting_practice/budgeting_testing/budgeting_testing/settings.py
+++ budgeting_practice/budgeting_testing/budgeting_testing/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m&^x!^-hlg7trxv0wq8wd4#sax+ayx^g3a%bvfpruc0-(u(+fp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -122,3 +120,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
